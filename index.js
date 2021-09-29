860225ffbee367f32607a1675142a6e6f9c105a5
John Youngblood <jsy@sehqc.com>
2021-09-28 17:27:47 -0700

    adding axios

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 13337;
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 13337;
+//
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+//
+// app.listen(PORT, () => {
+//   console.log(`Example app listening at http://localhost:${PORT}`)
+// })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+console.log(Buffer.from(process.env.YAHOO_CLIENT_ID+':'+process.env.YAHOO_CLIENT_SECRET).toString('base64'));
